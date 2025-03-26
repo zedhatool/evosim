6933bfeeadf3b205ee438064456cabc9084c57d4
Zed <zedhatool@gmail.com>
2025-03-26 15:09:24 -0700

    Figured out that everything was alright with the original scheme. Implemented some major changes, including an option to simulate the top-level game as a game of chicken where group strategies are determined by the level of cooperation in the group. I think we should work on implementing something like that for the original BCH model too. I also have an idea about how to implement a two-level replicator dynamics, but I need to work on that.

--- deterministic_sim.cpp
+++ deterministic_sim.cpp
@@ -21,7 +21,8 @@ const float GROUP_CONFLICT_CHANCE (0.25); //value from BCH. We may need to have 
 const int INITIAL_GROUPS = 100; //I randomly chose ten. We can change this later
 const float INSTITUTIONAL_CHANGE_CHANCE (0.1); //benchmark from BCH
 const float TIME_STEP (0.1); //time step for RK4 integration
-const int N_ITERATIONS (10);
+const int N_ITERATIONS (1);
+const float INDIVIDUAL_MUTATION_RATE (0.005);
 
 class Group {
 /*
@@ -31,8 +32,8 @@ class Group {
 */
 public:
     float proportionCooperative;
-    float taxRate;
     float segmentationRate;
+    float groupPayoff;
 
     /*
         Constructors
@@ -40,10 +41,12 @@ public:
     Group() {
         proportionCooperative = 0;
         segmentationRate = 0;
+        groupPayoff = 0;
     }
-    Group(float pCoop, float sRate) {
+    Group(float pCoop, float sRate, float gPayoff) {
         proportionCooperative = pCoop;
         segmentationRate = sRate;
+        groupPayoff = gPayoff;
     }
 
     float getPropCoop() {
@@ -57,6 +60,12 @@ public:
     }
     void setSegRate(float newSeg) {
         segmentationRate = newSeg;
+    }
+    void setPayoff(float newPayoff) {
+        groupPayoff = newPayoff;
+    }
+    float getPayoff() {
+        return groupPayoff;
     }
 };
 
@@ -75,7 +84,8 @@ float replicatorEquation(float pCoop, float seg) {
 
     return pCoop * (seg * R + (1 - seg) * (pCoop * R + (1 - pCoop) * S)
         - pCoop * (seg * R + (1 - seg) * (pCoop * R + (1 - pCoop) * S))
-        - (1 - pCoop) * (seg * P + (1 - seg) * (pCoop * T + (1 - pCoop) * P))); //I think I got that right
+        - (1 - pCoop) * (seg * P + (1 - seg) * (pCoop * T + (1 - pCoop) * P)))
+        - pCoop * INDIVIDUAL_MUTATION_RATE + (1 - pCoop) * INDIVIDUAL_MUTATION_RATE; //I think I got that right
 }
 
 /*
@@ -99,7 +109,25 @@ void playWithinGroup(Group& group, float dt, int N) {
     Since the groups now have infinite size, we just compare which one is more cooperative
 */
 void playBetweenGroups(Group& groupOne, Group& groupTwo) {
-    if (groupOne.getPropCoop() >= groupTwo.getPropCoop()) {//break ties in favour of group one
+    float S = 0; //Sucker's payoff
+    float T = 1; //Temptation to defect
+    float R = 0.6; //Reward for cooperation
+    float P = 0.3; //Punishment for mutual defection
+    float pCoopOne = groupOne.getPropCoop();
+    float segOne = groupOne.getSegRate();
+    float pCoopTwo = groupTwo.getPropCoop();
+    float segTwo = groupTwo.getSegRate();
+
+    float expectedPayoffOne = pCoopOne * (segOne * R + (1 - segOne) * (pCoopOne * R + (1 - pCoopOne) * S))
+    + (1 - pCoopOne) * (segOne * P + (1 - segOne) * (pCoopOne * T + (1 - pCoopOne) * P));
+
+    float expectedPayoffTwo = pCoopTwo * (segTwo * R + (1 - segTwo) * (pCoopTwo * R + (1 - pCoopTwo) * S))
+    + (1 - pCoopTwo) * (segTwo * P + (1 - segTwo) * (pCoopTwo * T + (1 - pCoopTwo) * P));
+
+    expectedPayoffOne *= (1 - segOne * segOne);
+    expectedPayoffTwo *= (1 - segTwo * segTwo);
+
+    if (expectedPayoffOne >= expectedPayoffTwo) {//break ties in favour of group one
         groupTwo.setPropCoop(groupOne.getPropCoop());
         groupTwo.setSegRate(groupOne.getSegRate());
     }
@@ -107,6 +135,78 @@ void playBetweenGroups(Group& groupOne, Group& groupTwo) {
         groupOne.setPropCoop(groupTwo.getPropCoop());
         groupOne.setSegRate(groupTwo.getSegRate());
     }
+}
+
+/*
+    Alternative way of thinking about top-level game as a game of chicken. Will implement this for
+    the stochastic model as well. The groups compete over a resource of value V. A fight costs F > V.
+    If they both back down, they split V evenly. If both fight, they get (V - F) / 2. If one fights
+    and the other backs down, the winner gets all of V and the loser gets nothing. Then,
+    we do updating by payoff on a group level as well! We can determine a group's type
+    by how cooperative/defective they are. Defective groups fight, cooperative
+    groups back down.
+*/
+void playChickenGame(Group& groupOne, Group& groupTwo) {
+    float V (0.5); //value of the resource
+    float F (1.0); //cost of a fight
+
+    if (groupOne.getPropCoop() < 0.5 && groupTwo.getPropCoop() < 0.5) { //let's have a war!
+        groupOne.setPayoff(0.5 * (V - F));
+        groupTwo.setPayoff(0.5 * (V - F));
+    }
+    else if (groupOne.getPropCoop() < 0.5 && groupTwo.getPropCoop() >= 0.5) { //group one takes it all
+        groupOne.setPayoff(V);
+        groupTwo.setPayoff(0);
+    }
+    else if (groupOne.getPropCoop() >= 0.5 && groupTwo.getPropCoop() >= 0.5) { //split evenly
+        groupOne.setPayoff(0.5 * V);
+        groupTwo.setPayoff(0.5 * V);
+    }
+    else {
+        groupOne.setPayoff(0);
+        groupTwo.setPayoff(V);
+    }
+}
+
+std::vector<Group> replicateGroups(std::vector<Group>& world) { //produce a new world
+    std::mt19937 randomizer;
+    auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    randomizer.seed((unsigned long)seed);
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+
+    //do stochastic updating like in BCH model for players, but for groups
+    float total (0);
+    for (int i (0); i < world.size(); ++i) {
+        total += world[i].getPayoff();
+    } //calculate total payoff
+
+    int worldSize = world.size();
+    std::vector<int> subintervals;
+    std::vector<float> weights;
+
+    for (size_t j = 0; j <= worldSize; ++j) {
+        subintervals.push_back(j);
+    }
+    for (size_t k = 0; k < worldSize; ++k) {
+        float pj = world[k].getPayoff();
+        weights.push_back(pj / total);
+    }
+
+    //This defines a probability mass function on subintervals given by the indices of groups
+    std::piecewise_constant_distribution<> d(subintervals.begin(), subintervals.end(), weights.begin());
+
+    int spreaders (0);
+    std::vector<Group> newWorld; //the thing we're gonna return
+
+    while (spreaders < worldSize) {
+        float randResult = d(randomizer);
+        int randIndex = (int) randResult;
+
+        newWorld.push_back(world[randResult]);
+        ++spreaders;
+    }
+
+    return newWorld;
 }
 
 void mutate(Group& group) {
@@ -136,13 +236,16 @@ int main() {
 
     std::uniform_real_distribution<> dis(0.0, 1.0); //uniform distribution on [0, 1]
     std::vector<Group> world;
+    std::vector<Group> dummyWorld;
 
     std::binomial_distribution<> war (INITIAL_GROUPS, GROUP_CONFLICT_CHANCE); //how big is the war
 
-    float ic (0);
+    //float ic (0);
+    //float ic2 (0);
     for (int i (0); i < INITIAL_GROUPS; ++i) {
-        ic = dis(randomizer);
-        Group group (ic, 0);
+        //ic = dis(randomizer);
+        //ic2 = dis(randomizer);
+        Group group (0.0, 0.0, 0.0);
         world.push_back(group);
     }
 
@@ -181,7 +284,8 @@ int main() {
         int l (0); //this logic lets us use one loop instead of two
         while (l < world.size()) { //Let's have a war!
             if (l < warSize - 1) {
-                playBetweenGroups(world[l], world[l+1]);
+                //playBetweenGroups(world[l], world[l+1]);
+                playChickenGame(world[l], world[l+1]);
                 pCoop += (world[l].getPropCoop() + world[l+1].getPropCoop());
                 avgSRate += (world[l].getSegRate() + world[l+1].getSegRate());
                 l += 2;
@@ -192,6 +296,9 @@ int main() {
                 ++l;
             }
         }
+
+    dummyWorld = replicateGroups(world);
+    world = dummyWorld;
 
     pCoop /= INITIAL_GROUPS;
     avgSRate /= INITIAL_GROUPS;
