d2fb896343f387c71b46aad9fdf564b24f46600d
Zed <zedhatool@gmail.com>
2025-04-03 21:20:12 -0700

    Power came back on so I am committing my changes. Currently throwing a segmentation fault and I have an idea why but not totally sure.

--- pde_model.cpp
+++ pde_model.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
-
+/*
+    Euler's number
+*/
+const float E = 2.71828;
+const float PI = 3.14159;
 /*
     Payoffs for lower level game
 */
@@ -22,46 +27,82 @@ float getVelocity(float c) {
 }
 
 float getDiffusion(float c) {
-    return c * (1 - c) * (c * V + 0.5 * (1 - c) * (V - F))
+    return c * (1 - c) * (c * V + 0.5 * (1 - c) * (V - F));
 }
 
 /*
-    Do two-stage Lax-Wendroff scheme on the advection term and Crank-Nicolson for the diffusion part
+    Do two-stage Lax-Wendroff scheme on the advection term and FTCS for the diffusion part
 */
 int main() {
     //Xf = 1
     int Tf;
-    std::cout >> "How Long to run the model?" >> std::endl;
-    std::cin << Tf;
+    std::cout << "How Long to run the model?" << std::endl;
+    std::cin >> Tf;
 
     float dt (0.01);
     float dx (0.01);
 
-    int xPoints = (int) (1 / dx);
+    int xPoints = 1 + (int) (1 / dx);
+    int tPoints = 1 + (int) (Tf / dt); //I added 1 + [...] thinking it would fix a segfault but it did not...
 
-    float U[Tf][xPoints]; //solution matrix
+    float U[tPoints][xPoints]; //solution matrix
+    float variance = 0.001;
+    float mean = 0.5;
+
+    //define initial condition for solution U, just using a sharp Gaussian for initial function
+    for (int y (0); y < xPoints; ++y) {
+        U[0][y] = (1 / (std::sqrt(2 * PI * variance))) * std::pow(E, -0.5 * (y - mean) * (y - mean) / variance);
+    }
+
     float uhalfLaxPlus (0);
     float uhalfLaxMinus (0);
     float fhalfLaxPlus (0);
     float fhalfLaxMinus (0);
-    float uhalfCrankPlus (0);
-    float uhalfCrankMinus (0);
+    float dHalfFTCSMinus (0);
+    float dHalfFTCSPlus (0);
 
-    for (int t (1); t < Tf; ++t) {
+    for (int t (1); t < tPoints; ++t) {
         for (int x (1); x < xPoints - 1; ++x) {
-            //Lax-Wendroff Step
-            uhalfLaxPlus = 0.5 * (U[t][x + 1] + U[t][x]) - (dt / dx) * (U[t][x + 1] * getVelocity(xPoints * dx + dx) - U[t][x] * getVelocity(xPoints * dx));
-            uhalfLaxMinus = 0.5 * (U[t][x - 1] + U[t][x]) - (dt / dx) * (U[t][x-1] * getVelocity(xPoints * dx - dx) - U[t][x] * getVelocity(xPoints * dx));
-            fhalfLaxPlus = uhalfLaxPlus * getVelocity(xPoints * dx + 0.5 * dx);
-            fhalfLaxMinus = uhalfLaxMinus * getVelocity(xPoints * dx - 0.5 * dx);
+            //Lax-Wendroff Computation
+            uhalfLaxPlus = 0.5 * (U[t][x + 1] + U[t][x]) - (dt / dx) * (U[t][x + 1] * getVelocity(x * dx + dx) - U[t][x] * getVelocity(x * dx));
+            uhalfLaxMinus = 0.5 * (U[t][x - 1] + U[t][x]) - (dt / dx) * (U[t][x-1] * getVelocity(x * dx - dx) - U[t][x] * getVelocity(x * dx));
+            fhalfLaxPlus = uhalfLaxPlus * getVelocity(x * dx + 0.5 * dx);
+            fhalfLaxMinus = uhalfLaxMinus * getVelocity(x * dx - 0.5 * dx);
 
-            //Crank-Nicolson Step
+            //FTCS Computation
+            dHalfFTCSPlus = getDiffusion(x * dx + 0.5 * dx);
+            dHalfFTCSMinus = getDiffusion(x * dx - 0.5 * dx);
 
-            U[t + 1][x] += U[t][x] - (dt / dx) * (fhalfLaxPlus - fhalfLaxMinus);
-
-
+            U[t + 1][x] = U[t][x] + (dt / (dx * dx)) *
+            (dHalfFTCSPlus * (U[t][x + 1] - U[t][x]) - dHalfFTCSMinus * (U[t][x] - U[t][x-1]))
+            - (dt / dx) * (fhalfLaxPlus - fhalfLaxMinus);
         }
     }
 
+    //define the file output stuff
+    std::ofstream outf ("pde_out.csv");
+
+    if (!outf) {
+        std::cerr << "Well, cock. Some C++ nonsense means the file output didn't work.\n";
+        return 1;
+    }
+
+    outf << "Time" << ",";
+
+    for (int k (0); k < xPoints; ++k) {
+        outf << k * dx << ",";
+    }
+
+    outf << std::endl;
+
+    for (int i (0); i < tPoints; ++i) {
+        outf << i * dt << ",";
+        for (int j (0); j < xPoints; ++j) {
+            outf << j * dx << ",";
+        }
+        outf << std::endl;
+    }
+
+    outf.close();
     return 0;
 }
